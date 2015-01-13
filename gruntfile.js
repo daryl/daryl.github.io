473454358c2ba8c3614aaa70443a3885bbb93e4b
Daryl Ginn <me@daryl.im>
2015-01-13 06:56:06 +0000

    Styling/Font fixes

--- gruntfile.js
+++ gruntfile.js
@@ -12,6 +12,9 @@ module.exports = function(grunt) {
     },
     myth: {
       default: {
+        options: {
+          compress: true
+        },
         files: {
           'public/dist/bundle.css': 'app/styles/app.css'
         }
